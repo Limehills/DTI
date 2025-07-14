a0df0f5afb02aa0131bd7a22bf1c4d86255cb3e9
unknown <manipisalbert888@gmail.com>
2025-07-14 15:27:16 +0800

    hi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DtrController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/dtr', [DtrController::class, 'index'])->name('dtr.index');
+    Route::post('/dtr/time-in', [DtrController::class, 'timeIn'])->name('dtr.timein');
+    Route::post('/dtr/time-out', [DtrController::class, 'timeOut'])->name('dtr.timeout');
 });
