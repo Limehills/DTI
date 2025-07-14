7a464927da09126bbe52a65becde428511e11aa1
Aljen Mondarte <mondarte.aljen31@gmail.com>
2025-07-14 15:02:27 +0800

    register

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\RegisterController;
+
+Route::get('/register', [RegisterController::class, 'show'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
