990c6e40f0cff9dc6af5928fc9b7936920c3032d
unknown <cyrhildwight@gmail.com>
2025-07-14 15:24:32 +0800

    login form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'getLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'postLogin']);
 });
+
+
+
+
+
+
